9705d61163915d7fdef99f5fc13a4ddc6bd42a0d
karishmalodhi <karishmalodhicse.aiml2024@agra.sharda.ac.in>
2026-06-24 00:50:06 +0530

    Updated home page UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,46 @@
-let count= 0;
-const buttons= document.querySelectorAll(".cart-btn");
-const cartBtn= document.getElementById("cart-btn");
+let count = 0;
+
+const cartBtn = document.getElementById("cart-btn");
+
+const buttons = document.querySelectorAll(".cart-btn");
+
 buttons.forEach(button => {
+
     button.addEventListener("click", () => {
+
         count++;
-        cartBtn.textContent = "Cart (" + count + ")";
+
+        cartBtn.innerText = `Cart (${count})`;
+
         alert("Product Added To Cart");
+
     });
 
 });
-function login() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+const searchBtn = document.getElementById("search-btn");
 
-    if(email === "" || password === "") {
-        alert("Please fill all fields");
+searchBtn.addEventListener("click", () => {
+    let searchValue = document.querySelector(".search-box input").value;
+
+    if(searchValue === ""){
+        alert("Please enter something to search");
     } else {
-        alert("Login Successful");
+        alert(`Searching for ${searchValue}`);
     }
-}
+});
+const categories = document.querySelectorAll(".category");
+
+categories.forEach(category => {
+    category.addEventListener("click", () => {
+        alert(`${category.innerText} selected`);
+    });
+});
+document.getElementById("shop-now").addEventListener("click", () => {
+    window.scrollTo({
+        top: 500,
+        behavior: "smooth"
+    });
+});
+document.getElementById("theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
